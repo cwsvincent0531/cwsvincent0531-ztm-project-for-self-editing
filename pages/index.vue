5c668700a01849ee8d2fe51c3a63951fb83822f3
cwsvincent0531 <cwsvincent0531@gmail.com>
2022-08-21 10:56:57 +0800

    Edited index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,49 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <div class="flex justify-center mb-3 p-4">
+          <!--White Card-->
+          <div class="rounded-lg shadow-md bg-white max-w-xl">
+            <div class="border-gray-300 p-4">
+              <img class="w-24 h-24 mt-4 mb-4 rounded-full mx-auto" src="https://media-exp1.licdn.com/dms/image/C4D03AQGXx_BYblySrQ/profile-displayphoto-shrink_800_800/0/1654308890710?e=1666224000&v=beta&t=q3amb1NfCSukX8QKEL3HzX74LVEJMCkk3B-TDOdnW5I" alt="" width="384" height="512"/>
+              <div class="flex justify-center">
+                <h1 class="text-4xl font-extrabold">CHAN WIN SON</h1>
+              </div>
+              <div class="flex justify-center mb-2"></div>
+                <p class="text-lg mx-10 tracking-tight">A pharmacist with a passion for data science.</p>
+              </div>
+              <!--Facebook button-->
+              <div class="flex justify-center mt-3 mb-5">
+                <button>
+                  <a href="link to be added" class="button">
+                    <svg
+                    class="mx-1 w-6 h-6 text-blue-600 fill-current hover:bg-blue-100 rounded shadow"
+                    title="Facebook"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24">
+                    <path
+                    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                    ></path>
+                    </svg>
+                  </a>
+                </button>
+                <!--LinkedIn button-->
+                <button>
+                  <a href="link to be added" class="button">
+                    <svg
+                    class="mx-1 w-6 h-6 text-blue-500 fill-current hover:bg-gray-100 rounded shadow"
+                    title="LinkedIn"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512">
+                    <path
+                    d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                    ></path>
+                    </svg>
+                  </a>
+                </button>
+            </div>
+            </div> 
+          </div>
+        </div>
     </div>
-  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: [
+    './pages/index.vue',
   ],
   theme: {
     fontFamily: {
